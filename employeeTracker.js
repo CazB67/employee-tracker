830a64a7142fdd49b1523c5bc6ceb7f1b2809cf1
Caroline <caroline.portilla@gmail.com>
2020-05-20 18:12:37 +0800

    comment

--- employeeTracker.js
+++ employeeTracker.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'OscarChloeLeo1',
+    password: '',
     database: 'employee_tracker_db'
 })
 
